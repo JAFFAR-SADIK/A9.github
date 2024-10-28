d209b764e682b4504ea755f351632f2255948c34
JAFFAR-SADIK <wahida786sk@gmail.com>
2024-10-28 12:34:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("menuButton").addEventListener("click", function() {
+    const menuItems = document.getElementById("menuItems");
+    menuItems.classList.toggle("hidden");
+});
